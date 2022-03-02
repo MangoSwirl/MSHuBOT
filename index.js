29f6feead357359981847d7b3b93ee13892ba7d2
MangoSwirl <collin.camerono@gmail.com>
2022-03-02 14:33:18 -0800

    Add autocomplete functionality

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const faq = require('./faq.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES] });
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('Logged in as ' + client.user.tag);
 
     // Create slash commands
@@ -20,26 +20,72 @@ client.on('ready', () => {
     //         }
     //     ]
     // })
+
+    // const originalCommand = await (await client.application.commands.fetch()).first()
+
+    // client.application.commands.edit(originalCommand, {
+    //     options: [
+    //         {
+    //             type: "STRING",
+    //             name: "question",
+    //             description: "Question",
+    //             required: false,
+    //             autocomplete: true
+    //         }
+    //     ]
+    // })
+
+    // console.log(await (await client.application.commands.fetch()).first().options)
 });
 
 client.on('interactionCreate', (interaction) => {
+    if (interaction.isAutocomplete()) {
+        switch (interaction.commandName) {
+            case 'faq':
+                const value = interaction.options.get("question").value;
+
+                let filteredOptions;
+                if (!value || value.length === 0) {
+                    filteredOptions = Object.keys(faq)
+                } else {
+                    filteredOptions = Object.keys(faq).filter(key => key.toLowerCase().startsWith(value.toLowerCase()))
+                }
+
+                console.log(filteredOptions)
+
+                // Trim to 25 items
+                if (filteredOptions.length > 25) {
+                    filteredOptions = filteredOptions.slice(0, 25);
+                }
+
+                console.log(filteredOptions)
+
+                interaction.respond(filteredOptions.map(option => ({ name: option, value: option })));
+        }
+    }
+
+
     // We only want commands.
     if (!interaction.isApplicationCommand()) return;
 
-    const question = interaction.options.get("question", false)
+    switch (interaction.commandName) {
+        case 'faq':
+            const question = interaction.options.get("question", false)
 
-    if (!question) {
-        // If there isn't a question, show the full list.
-        interaction.reply("**Frequently Asked Questions:**\n" + Object.values(faq).map(question => `**${question.title}** \n${question.message}`).join("\n\n"));
-    } else {
-        // If there is a question, show the answer.
-        const answer = faq[question.value];
-        if (!answer) {
-            interaction.reply({ content: "I don't have an answer for that question.", ephemeral: true });
-            return;
-        }
+            if (!question) {
+                // If there isn't a question, show the full list.
+                interaction.reply("**Frequently Asked Questions:**\n" + Object.values(faq).map(question => `**${question.title}** \n${question.message}`).join("\n\n"));
+            } else {
+                // If there is a question, show the answer.
+                const answer = faq[question.value];
+                if (!answer) {
+                    interaction.reply({ content: "I don't have an answer for that question.", ephemeral: true });
+                    return;
+                }
 
-        interaction.reply({ content: `**${answer.title}** \n${answer.message}`, files: answer.image ? [{attachment: answer.image}] : [] });
+                interaction.reply({ content: `**${answer.title}** \n${answer.message}`, files: answer.image ? [{attachment: answer.image}] : [] });
+            }
+            break;
     }
 });
 
